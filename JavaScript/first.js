148dbbfb7ba2c9eb44fc94f5ab8b7f162a689ddb
edupointktm <edupointktm@gmail.com>
2023-10-08 10:41:48 +0545

    Javascript 1

---
+++ JavaScript/first.js
@@ -0,0 +1,7 @@
+
+var a = prompt("Enter you name : ")
+var add = "Janakpur"
+
+// document.write(a)
+// document.write("<h1> My Name is : " + a +  " and address is : " + add +"</h1>")
+document.write(`<h1> My Name is : ${a} <h1>`)
